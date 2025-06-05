37245ced0fc9d8ebd6227dae06a986c226e3cb8c
Sho Okumoto <lebesgue0118@gmail.com>
2025-06-05 10:34:12 +0900

    WIP

--- python_ml_examples/generative_models/flow_matching_moon.py
+++ python_ml_examples/generative_models/flow_matching_moon.py
@@ -10,13 +10,14 @@ from python_ml_examples.generative_models.flow_matching.flow_matching import (
 )
 from python_ml_examples.generative_models.flow_matching.utils import (
     generate_flow_matching_mp4,
+    generate_gaussian_mixture,
 )
 
 
 n_batch_samples = 512
 dim = 2
 
-# Create a fullyt connected neural network model for flow matching
+# Create a fullt connected neural network model for flow matching
 model = Mlp(n_dim=dim).cuda()
 
 # Choose one of the schedulers to use
@@ -27,11 +28,22 @@ flow_matching = FlowMatching(
     model=model,
     scheduler=scheduler,
 )
-x_1 = torch.tensor(make_moons(n_samples=n_batch_samples, noise=0.05)[0]).float().cuda()
+x_1 = (
+    torch.tensor(
+        generate_gaussian_mixture(
+            300,
+            n_clusters=3,
+            means=[[0.0, 0.0], [2.0, 2.0], [-2.0, -2.0]],
+            variances=[[0.4, 0.4], [0.4, 0.4], [0.4, 0.4]],
+        )
+    )
+    .float()
+    .cuda()
+)
 flow_matching.train(x_1, parameter_updating_steps=10000)
 path = flow_matching.sample_path(200, num_ode_steps=100)
 
 # Save generated path as mp4 file
 paths = {}
 paths["LinearScheduler"] = path
-generate_flow_matching_mp4(paths, filename="flow_matching_moon.mp4")
+generate_flow_matching_mp4(paths, filename="flow_matching_moon.gif")
